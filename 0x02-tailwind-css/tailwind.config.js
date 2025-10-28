c6e799ffffe4ce0328372ed239750cacef6cf55a
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-10-28 19:16:41 +0300

    remoced html outsiede

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./*.{html,js}",     // looks for HTML and JS files in the root folder
+    "./src/**/*.{html,js}" // still scans everything inside src
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+WWW
